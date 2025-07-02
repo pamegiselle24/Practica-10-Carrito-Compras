3c741cde30d205fa5156b0556a0efea0f2ef878e
TriLuli <pamegiselle24@gmail.com>
2025-07-01 23:00:35 -0300

    Configuración inicial de la aplicacion

---
+++ src/CarritoApp.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+
+export const CarritoApp = () => {
+  return (
+    <>
+      <NavBar> </NavBar>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </>
+  );
+};
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+export const NavBar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Carrito
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Compras
+              </a>
+            </li>
+          </ul>
+          <a class="nav-link">🛒</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
