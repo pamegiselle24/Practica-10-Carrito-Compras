e2c2f5b842ca3949548471d3a8f6e17ceef7293a
Pame <pamegiselle24@gmail.com>
2025-07-12 00:35:46 -0300

    Agregué estructura de contextos: ProductosContext y CarritoContext

--- src/pages/CarritoPage.jsx
+++ src/pages/CarritoPage.jsx
@@ -1,3 +1,29 @@
 export const CarritoPage = () => {
-  return <div>Carritosssss</div>;
+  return (
+    <>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col">Eliminar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="d-grid gap-2">
+        <button type="button" className="btn btn-primary">
+          Comprar
+        </button>
+      </div>
+    </>
+  );
 };
